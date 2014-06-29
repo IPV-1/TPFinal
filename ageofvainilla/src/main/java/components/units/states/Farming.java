c9abc7b4a27f1d4800d5c92323a9e86fc4c37a19
Mariano <marianolinares92@gmail.com>
2014-06-29 03:10:30 -0300

    Recolectar recursos tranki

--- ageofvainilla/src/main/java/components/units/states/Farming.java
+++ ageofvainilla/src/main/java/components/units/states/Farming.java
@@ -1,5 +1,7 @@
 package components.units.states;
 
+import utils.FarmingTimer;
+
 import com.uqbar.vainilla.DeltaState;
 import components.units.MovingUnit;
 import components.units.Unit;
@@ -8,15 +10,21 @@ import components.units.resources.Resource;
 public class Farming extends UnitState {
 	
 	private Resource resource;
+	private FarmingTimer timer = new FarmingTimer();
 	
 	public Farming(Unit unit) {
 		resource = (Resource) unit;
+		timer.setLimit(1);
 	}
 
 	@Override
-	public void update(MovingUnit movingUnit, DeltaState deltaState) {
+	public void update(MovingUnit unit, DeltaState deltaState) {
+		timer.update(this, unit, deltaState);
+	}
+	
+	public void farm(MovingUnit unit) {
 		if(resource.getAmount() == 0) {
-			movingUnit.setState(new Waiting());
+			unit.setState(new Waiting());
 		} else {
 			resource.addResourceToPlayer();
 		}
